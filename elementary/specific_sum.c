ea723b34b553cd36c7c4e005a8a2053c41c3b1e0
Sanatan kafle <kaflesanatan100@gmail.com>
2019-12-28 20:33:10 +0545

    adding multiple of 3 and 5

--- elementary/specific_sum.c
+++ elementary/specific_sum.c
@@ -1,4 +1,4 @@
-/*Modify the previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=1*/
+/*Modify the previous program such that only multiples of three or five are considered in the sum*/
 
 #include <stdio.h>
 
