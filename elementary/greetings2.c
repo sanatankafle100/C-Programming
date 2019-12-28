7c3699cf89f12bc53cd0ba3a432da12c67e84e2c
Sanatan kafle <kaflesanatan100@gmail.com>
2019-12-28 18:51:19 +0545

    Program with error

---
+++ elementary/greetings2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void main(){
+	char name;
+	printf("Please Enter your name. \n");
+    scanf(" %c",&name);
+	if (name == 'Alice'|| name == 'Bob')
+     {
+         printf("The name is nice\n");
+     }
+}
+
