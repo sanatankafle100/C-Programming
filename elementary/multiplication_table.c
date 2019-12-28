c9827e20f55f628bf12c1b83686a78f27e79c27f
Sanatan kafle <kaflesanatan100@gmail.com>
2019-12-28 21:51:00 +0545

    Multiplication Table

--- elementary/multiplication_table.c
+++ elementary/multiplication_table.c
@@ -1,1 +1,16 @@
-/*Write a program that prints a multiplication table for numbers up to 12*/
+/*Write a program that prints a multiplication table for any user given number*/
+#include <stdio.h>
+
+int main(){
+	int num,i,value;
+
+	printf("Enter number whose multiplication table you want: ");
+	scanf("%d",&num);
+
+	for(i=1;i<=10;i+=1)
+	{
+		value=i*num;
+		printf("%d*%d=%d\n",num,i,value);
+	}
+	return 0;
+}
