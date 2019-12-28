2afc58436d5b5b4aa947012ef5fca04d4bc2d210
Sanatan kafle <kaflesanatan100@gmail.com>
2019-12-28 13:02:40 +0545

    Calculating Sum of numbers multiple of 3 and 5

---
+++ elementary/specific_sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int sum=0,n;
+	printf("Enter a Number:");
+	scanf("%d",&n);
+	while(n>0){
+		if(n%3==0 || n%5==0){
+			sum+=n;
+			n-=1;
+		}
+		else{
+			n-=1;
+		}
+
+	}
+	printf("The sum of numbers  which are multiple of 3 are 5 is %d\n",sum);
+	return 0;
+}
