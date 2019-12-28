edd9d4e0c841abbd86e1186a232e89061ec40727
Sanatan kafle <kaflesanatan100@gmail.com>
2019-12-28 12:53:14 +0545

    Calculating Sum of n natural numbers

---
+++ elementary/sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int sum=0, n;
+	printf("Please Enter a number. We'll calculate the sum of 1 to the number you entered.\n");
+	scanf("%d",&n);
+	while(n>0){
+		sum+=n;
+		n-=1;
+	}
+	printf("%d is the sum.\n",sum);
+}
