749ea801e5fedb68c5e9bd75c57019c3de00da0d
Sanatan kafle <kaflesanatan100@gmail.com>
2019-12-28 20:30:08 +0545

    Greeting the user

--- elementary/greeting.c
+++ elementary/greeting.c
@@ -1,4 +1,4 @@
-/*Write a program that asks the user for their name and greets them with their name.*/
+/*Write a program that asks the user for their name and greets them with their name .*/
 
 #include <stdio.h>
 
