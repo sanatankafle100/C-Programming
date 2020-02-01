d939bf7d891b46de83a06c6abf635b71781673ed
Sanatan kafle <kaflesanatan100@gmail.com>
2020-02-01 13:52:50 +0545

    printing largest element

--- list_strings/largest.c
+++ list_strings/largest.c
@@ -1,26 +1,38 @@
+
 /*Write a function that returns the largest element in a list.*/
 #include <stdio.h>
 
-int largest();
-int main(){
-	largest();
+float largest(float array[]);
+int main()
+{
+  float lists[50],ans;
+  int i,num;
+
+  printf("Enter the length of the list. ");
+  scanf("%d",&num);
+
+  for(i=0;i<num;++i)
+  {
+    printf("Enter the element of the list: ");
+    scanf("%f",&lists[i]);
+  }
+  ans=largest(lists);
+  printf("%.2f",ans);
+  return 0;
 }
 
-int largest()
+float largest(float array[])
 {
-	int num,i;
-	float array[20];
-	printf("Enter the length of array:  ");
-	scanf("%d",&num);
-	for (i = 0; i < num; ++i) 
-	{
-        printf("Enter number%d: ", i + 1);
-        scanf("%f", &array[i]);
+  int i,num;
+  printf("Enter the length of the list. ");
+  scanf("%d",&num);
+
+  for(i=0;i<num;++i)
+  {
+    if (array[0] < array[i])
+      array[0] = array[i];
     }
-    for (i = 1; i < num; ++i) 
-    {
-        if (array[0] < array[i])
-            array[0] = array[i];
-    }
-    printf("%.2f",array[0]);
+  return array[0];
 }
+
+
