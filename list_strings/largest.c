538d1dc205540b31b844fff82c35b4ec8e306dd6
Sanatan kafle <kaflesanatan100@gmail.com>
2020-02-02 09:34:17 +0545

    largest element of array

--- list_strings/largest.c
+++ list_strings/largest.c
@@ -4,11 +4,13 @@
  
 double largest();
 int main(){
-  double largest1, largest2;
-  double array1[] = {-1, -2, -54.1, -23};
+  double largest1, largest2, largest3;
+  double array1[] = {-1, -2, -54, -23};
   double array2[] = {1, 23, 32};
+  double array3[]={1};
+  double array4[]={1.1, 23.1, 2321.2};
  
-  printf("Testing for the first array containing 1, 2, 54.12, 23\n");
+  printf("Testing for the first array containing -1, -2, -54, -23\n");
   largest1 = largest(array1, 4);
   assert(largest1<0);
   assert(largest1 == -1);
@@ -18,11 +20,17 @@ int main(){
   largest2 = largest(array2, 3);
   assert(largest2 == 32);
 
-   
+  printf("Testing for the third array containing 1\n");
+  largest3=largest(array3,1);
+  assert(largest3 == 1);
 
-  printf("Both of the test passed successfully\n");
+  printf("Testing for the fourth array containing 1.1, 23.1, 2321.2\n");
+  largest3=largest(array4,3);
+  assert(largest3 == 2321.2);
+
+
+  printf("All of the test passed successfully\n");
  
-
 }
  
 double largest(double array[], int size)
